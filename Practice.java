dfc18f20496f560cf9c81aac06f691b7608e0d45
Ar3vind <aravindbinuraj05@gmail.com>
2025-06-14 15:44:46 +0100

    First commit

---
+++ Practice.java
@@ -0,0 +1,25 @@
+public class Practice {
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Welcome to Java Practice!");
+
+        // Simple addition
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+
+        // Loop example
+        System.out.println("Counting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        // Conditional example
+        if (sum > 10) {
+            System.out.println("The sum is greater than 10.");
+        } else {
+            System.out.println("The sum is 10 or less.");
+        }
+    }
+}
